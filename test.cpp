8f6e568a79029a9474bf5327258e941c0f6c7cb1
denvyworking <denis04012006sobolev@gmail.com>
2025-07-20 14:36:18 +0300

    Revert "добавил class"
    This reverts commit fe974ec79edc345163b021ef62d139b5d7cb6d7b.

--- test.cpp
+++ test.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include <string>
-class NewReatures{
-private:
-    std::string message;
-public:
-    NewReatures(std::string str){
-        message = str;
-    }
-    
-    void Print(){
-        std::cout << this->message << std:: endl;
-    }
-};
 
 int main(){
     std::cout << "Test repository in Git" << std::endl;
     std::cout << "I want working with git" << std::endl;
     std::cout << "У меня получилось закоммитить !!!" << std::endl;
-    NewReatures s = NewReatures("help");
-    s.Print();
     return 0;
 }
-
