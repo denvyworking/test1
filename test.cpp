b7216f6a084a440e4d77fa63991bc94475eee49c
denvyworking <denis04012006sobolev@gmail.com>
2025-07-20 14:02:15 +0300

    добавил сообщение в 5 строке

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@
 int main(){
     std::cout << "Test repository in Git" << std::endl;
     std::cout << "I want working with git" << std::endl;
+    std::cout << "У меня получилось закоммитить !!!" << std::endl;
     return 0;
 }
